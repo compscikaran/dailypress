27f6a254ab4ffe0b86bb99d85b8d94c43bc79d99
Karan Gupta <gkaran184@gmail.com>
2018-12-14 12:36:37 +0530

    Changed masthead font
    Signed-off-by: Karan Gupta <gkaran184@gmail.com>

--- sejournal/settings.py
+++ sejournal/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'sejournal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sejournal',
+        'USER': 'postgres',
+        'PASSWORD': 'karan184',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
