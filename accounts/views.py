027badfc356a05d5ba4cadac93181bc7e666d210
Karan Gupta <gkaran184@gmail.com>
2018-12-20 10:28:58 +0530

    Added authorization key check on signup form
    Signed-off-by: Karan Gupta <gkaran184@gmail.com>

--- accounts/views.py
+++ accounts/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
+from .models import AuthorizationKey
 
 
 def signup(request):
     if request.method == 'POST':
-        if request.POST['password'] == request.POST['password2']:
-            if  User.objects.filter(username=request.POST['username']).count() is not 0:
-                return render(request, 'accounts/signup.html', {'error' : 'Username is Ttaken'})
+        if request.POST['authorization_key']:
+            if AuthorizationKey.objects.filter(key=request.POST['authorization_key']).count() is not 0:
+                if request.POST['password'] == request.POST['password2']:
+                    if  User.objects.filter(username=request.POST['username']).count() is not 0:
+                        return render(request, 'accounts/signup.html', {'error' : 'Username is taken'})
+                    else:
+                        user = User.objects.create_user(request.POST['username'], 
+                                                        request.POST['email'], request.POST['password'])
+                        auth.login(request, user)
+                        return redirect('all_articles')
+                else:
+                    return render(request, 'accounts/signup.html', {'error' : 'Passwords do not match'})
             else:
-                user = User.objects.create_user(request.POST['username'], 
-                                                request.POST['email'], request.POST['password'])
-                auth.login(request, user)
-                return redirect('all_articles')
+                return render(request, 'accounts/signup.html', {'error' : 'Contact administrator for valid authorization key'})
         else:
-            return render(request, 'accounts/signup.html', {'error' : 'Passwords do not match'})
+            return render(request, 'accounts/signup.html', {'error' : 'No authorization key provided'})
     else:
         return render(request, 'accounts/signup.html')
 
