bb802795bdf8d6535765f4a2b005c9c34d455707
Karan Gupta <gkaran184@gmail.com>
2018-12-20 13:34:39 +0530

    Changed name to dailypress
    Signed-off-by: Karan Gupta <gkaran184@gmail.com>

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from .views import counter
+from .views import counter, hits
 
 
 urlpatterns = [
     path('counter/', counter, name='counter'),
+    path('', hits, name='hits'),
 ]
--- analytics/views.py
+++ analytics/views.py
@@ -3,6 +3,7 @@ from .models import ArticleHit
 from authoring.models import Article
 from django.utils import timezone
 from django.http import JsonResponse
+from django.db.models import Count
 # Create your views here.
 
 def counter(request):
@@ -14,3 +15,10 @@ def counter(request):
     article_hit.save()
     response = 'Article ' + str(article.id) + ' Counted'
     return JsonResponse({'msg' : response})
+
+def hits(request):
+    list_hits = list(ArticleHit.objects.all().values('article').annotate(total=Count('article')).order_by('article'))
+    list_articles = list(Article.objects.all())
+    for i in range(0, len(list_hits)):
+        list_hits[i]['title'] = list_articles[i].title
+    return render(request, 'analytics/hits.html', {'list_hits' : list_hits})
